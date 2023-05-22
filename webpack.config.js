0f984eca6db4227ab5d1134678588b08f5be0b07
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-22 13:53:51 -0300

    Install Webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
