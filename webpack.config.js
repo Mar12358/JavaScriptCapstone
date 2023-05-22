6fd30883d98d65198df30c17d93280d64a28b238
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-22 13:58:32 -0300

    Use source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'My meals',
     }),
   ],
   output: {
