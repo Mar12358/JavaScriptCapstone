6126779c678adf7b06e57ca856b0b4087bb3e6d9
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-22 13:56:01 -0300

    Install HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     index: './src/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
