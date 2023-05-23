99fbd701b2e02f39023efe03577afb7230daa793
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-23 11:54:33 -0300

    Add like img, all elements info

--- src/modules/home_page.js
+++ src/modules/home_page.js
@@ -1,3 +1,5 @@
+import likeImg from '../like_img.png';
+
 const APIurl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=f';
 
 const getMeals = async () => {
@@ -11,17 +13,21 @@ const populateHTML = (meals) => {
   console.log(meals);
   for (let i = 0; i < 6; i += 1) {
     const id = meals[i].idMeal;
+    const mealName = meals[i].strMeal;
     const div = document.createElement('div');
-    const btn = document.createElement('button');
-
-    btn.innerHTML = 'Comments';
-    btn.className = 'comment-button';
-    btn.id = id;
+    const img = meals[i].strMealThumb;
 
     div.id = id;
     div.className = 'card-element';
-    div.innerHTML = meals[i].strMeal;
-    div.appendChild(btn);
+    div.innerHTML = `<img class="meal-img" src=${img} alt="">
+                    <div class="info">
+                      <span class="meal-name">${mealName}</span>
+                      <div class="like-div">
+                        <a href=""><img class="like-icon" src=${likeImg} alt=""></a>
+                        <span>5 likes</span>
+                      </div>
+                    </div>
+                    <button class="comment-button">Comments</button>`;
 
     container.appendChild(div);
   }
