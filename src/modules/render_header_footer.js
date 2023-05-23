35db85e1c3ee4f0eeb15c14e764760d0a05e3bb1
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-23 12:10:48 -0300

    Update render_headear_footer js

--- src/modules/render_header_footer.js
+++ src/modules/render_header_footer.js
@@ -4,35 +4,28 @@ const renderHeaderFooter = () => {
   const body = document.querySelector('body');
   const nav = document.createElement('nav');
   const section = document.createElement('section');
-  const footer = document.createElement('section');
+  section.className = 'container';
+  const footer = document.createElement('footer');
   const h1 = document.querySelector('h1');
-  nav.innerHTML = `<nav>
-                    <a><img id="header-logo" src=${headerLogo} alt="header_logo"></a>
-                    <ul>
-                      <li><a href="">Items(6)</a></li>
-                      <li><a href="">Categories</a></li>
-                      <li><a href="">Ingredients</a></li>
-                    </ul>
-                  </nav>`;
+  nav.innerHTML = `<a><img id="header-logo" src=${headerLogo} alt="header_logo"></a>
+                  <ul>
+                    <li><a id="items-link" href="">Items</a></li>
+                    <li><a href="">Categories</a></li>
+                    <li><a href="">Ingredients</a></li>
+                  </ul>`;
 
-  section.innerHTML = `<section class="container">
-                        <div class="food-list">
-                          food will be render here
-                        </div>
-                      </section>`;
+  section.innerHTML = `<div class="food-list">
+                       </div>`;
 
-  footer.innerHTML = `<footer>
-                        <p>Created by Microverse students by MIT license</p>
-                      </footer>`;
-
-  body.insertBefore(nav, h1);
-  body.appendChild(section);
-  body.appendChild(footer);
+  footer.innerHTML = '<p>Created by Microverse students by MIT license</p>';
   const btn = document.createElement('button');
   btn.innerHTML = 'Comments';
   btn.className = 'comment-button';
   btn.id = '52772';
-  body.appendChild(btn);
+  section.appendChild(btn);
+  body.insertBefore(nav, h1);
+  body.appendChild(section);
+  body.appendChild(footer);
 };
 
 export default renderHeaderFooter;
