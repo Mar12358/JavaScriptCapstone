08c19b5ef878e9f0b7221486a8c020ba0221fd5d
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-23 06:40:34 -0300

    Add module for Header and Footer (ES6), Add Style

---
+++ src/render_header_footer.js
@@ -0,0 +1,32 @@
+import headerLogo from './header_logo.png';
+
+const renderHeaderFooter = () => {
+  const body = document.querySelector('body');
+  const nav = document.createElement('nav');
+  const section = document.createElement('section');
+  const footer = document.createElement('section');
+  const h1 = document.querySelector('h1');
+  nav.innerHTML = `<nav>
+                    <a><img id="header-logo" src=${headerLogo} alt="header_logo"></a>
+                    <ul>
+                      <li><a href="">Items(6)</a></li>
+                      <li><a href="">Categories</a></li>
+                      <li><a href="">Ingredients</a></li>
+                    </ul>
+                  </nav>`;
+
+  section.innerHTML = `<section class="container">
+                        <div class="food-list">
+                          food will be render here
+                        </div>
+                      </section>`;
+
+  footer.innerHTML = `<footer>
+                        <p>Created by Microverse students by MIT license</p>
+                      </footer>`;
+  body.insertBefore(nav, h1);
+  body.appendChild(section);
+  body.appendChild(footer);
+};
+
+export default renderHeaderFooter;
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
+    renderHeaderFooter: './src/render_header_footer.js',
   },
   devtool: 'inline-source-map',
   devServer: {
