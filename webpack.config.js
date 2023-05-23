fae4feda24a778755361cc7c4e9a2da0a578f5db
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-23 05:46:14 -0300

    Add logo, Add File-Loader for png

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: 'file-loader',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
