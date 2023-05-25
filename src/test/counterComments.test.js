92d2bdda0e398a9d00648e210e193ee2bf68f0ab
Alishah Safdari <alishahsafdari@gmail.com>
2023-05-25 08:09:37 +0430

    Update the counter comment  file

--- src/test/counterComments.test.js
+++ src/test/counterComments.test.js
@@ -7,19 +7,19 @@ const items = [{ comment: 'Hi', creation_date: '2023-05-24', username: 'Ali' },
   { comment: 'Comments and more comments!', creation_date: '2023-05-24', username: 'Y' },
   { comment: 'New comment', creation_date: '2023-05-24', username: 'Martin' }];
 
-// const seconditems = [{ "comment": "Hi", "creation_date": "2023-05-24", "username": "Ali" },
-// { "comment": "Hi", "creation_date": "2023-05-24", "username": "Ali" }]
+const seconditems = [{ comment: 'Hi', creation_date: '2023-05-24', username: 'Ali' },
+  { comment: 'Hi', creation_date: '2023-05-24', username: 'Ali' }];
 
 describe('Here we test the counter comments', () => {
   test('Fist test: test the exact value ', () => {
     expect(counterComment(items)).toBe(6);
   });
 
-  test("Second test: test the not be equal", () => {
-      expect(counterComment(items)).not.toBe(3);
+  test('Second test: test the not be equal', () => {
+    expect(counterComment(items)).not.toBe(3);
   });
 
-  // test("Thired test: test the not be equal", () => {
-  //     expect(counterComment(seconditems)).toBe(2);
-  // });
+  test('Thired test: test the not be equal', () => {
+    expect(counterComment(seconditems)).toBe(2);
+  });
 });
